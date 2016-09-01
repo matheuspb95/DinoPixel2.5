3a640db317f98a012a54bf5c55b48cf1d7072504
matheuspb95 <matheuspb95@gmail.com>
2016-09-01 16:10:39 -0400

    Inital Commit

---
+++ Assets/Attack.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attack : MonoBehaviour {
+	public float Range;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.F)) {
+			
+		}
+	}
+}
